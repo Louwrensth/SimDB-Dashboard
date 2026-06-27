c427f5511a8cc177f842fca31f1f8c44b7d811be
Louwrens van Dellen <lthvd@protonmail.com>
2026-06-27 07:55:10 -0600

    nginx: add /scenarios/api proxy, rewrite and comment

--- docker/runtime-config-template.js
+++ docker/runtime-config-template.js
@@ -2,10 +2,10 @@ window.__SIMDB_RUNTIME_CONFIG__ = {
   // Modify runtime settings for the Dockerized dashboard.
   // nginx injects TOKENs at response time.
   servers: [
-    'SCHEME://PUBLIC_SIMDB_HOST:DASHBOARD_PORT/api',
+    'SCHEME://PUBLIC_SIMDB_HOST:DASHBOARD_PORT/scenarios/api',
   ],
-  defaultServer: 'SCHEME://PUBLIC_SIMDB_HOST:DASHBOARD_PORT/api',
+  defaultServer: 'SCHEME://PUBLIC_SIMDB_HOST:DASHBOARD_PORT/scenarios/api',
   serverConfig: {
-    'SCHEME://PUBLIC_SIMDB_HOST:DASHBOARD_PORT/api': { requiresAuth: false }
+    'SCHEME://PUBLIC_SIMDB_HOST:DASHBOARD_PORT/scenarios/api': { requiresAuth: false }
   }
 }
